fb5b76aae92b14e2521203ff60fe80a57cc679d0
prathibha padma (CapGem) <prathibha.padma@Defra.onmicrosoft.com>
2025-01-07 15:09:35 +0000

    Merged PR 73950: adding storage connection string

--- scripts/UpdateAppSettings.ps1
+++ scripts/UpdateAppSettings.ps1
@@ -10,10 +10,18 @@ Param(
     [string]$logicappsrefdataName,
     [Parameter(Mandatory = $true)]
     [string]$logicappsprocessorName,
+
     [Parameter(Mandatory = $true)]
-    [string]$appSettingsKey1,
+    [string]$storageAccountName,
     [Parameter(Mandatory = $true)]
-    [string]$appSettingsKey2
+    [string]$storageAccountRGName, 
+
+    [Parameter(Mandatory = $true)]
+    [string]$common_appSettingsKey,
+    [Parameter(Mandatory = $true)]
+    [string]$servicebus_appSettingsKey,
+    [Parameter(Mandatory = $true)]
+    [string]$storage_appSettingsKey
 )
 
 # Fetch the API Connection runtime URL
@@ -33,28 +41,44 @@ $serviceBusConnectionRuntimeUrl = az resource show `
     --query "properties.connectionRuntimeUrl" `
     -o tsv
 
-# Validate that both runtime URLs were fetched
-if (-not $apiConnectionRuntimeUrl -or -not $serviceBusConnectionRuntimeUrl) {
-    Write-Error "Error: One or both runtime URLs are missing."
-    Write-Error "API Connection: $apiConnectionRuntimeUrl, Service Bus: $serviceBusConnectionRuntimeUrl"
+# Fetch the storage account connection string
+Write-Host "Fetching Storage Account Connection String for '$storageAccountName'..."
+$connectionString = (az storage account keys list --resource-group $storageAccountRGName --account-name $storageAccountName --query "[0].value" -o tsv)
+
+if (-not $connectionString) {
+    Write-Error "Failed to fetch Storage Account Connection String for '$storageAccountName'."
+    exit 1
+}
+$connectionStringFormatted = "DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$connectionString;EndpointSuffix=core.windows.net"
+
+
+# Validate that runtime URLs and storage connection string were fetched
+if (-not $apiConnectionRuntimeUrl -or -not $serviceBusConnectionRuntimeUrl -or -not $connectionStringFormatted) {
+    Write-Error "Error: One or more required values are missing."
+    Write-Error "API Connection URL: $apiConnectionRuntimeUrl"
+    Write-Error "Service Bus URL: $serviceBusConnectionRuntimeUrl"
+    Write-Error "Storage Connection String: $connectionStringFormatted"
     exit 1
 }
 
 # Write-Host "Fetched API Connection Runtime URL: $apiConnectionRuntimeUrl"
 # Write-Host "Fetched Service Bus Connection Runtime URL: $serviceBusConnectionRuntimeUrl"
+# Write-Host "Storage Connection String: $connectionStringFormatted"
 
 # Update App Settings in the App Services
 Write-Host "Updating App Settings for Logic App: $logicappsrefdataName..."
-az webapp config appsettings set --name $logicappsrefdataName --resource-group $resourceGroupName --settings "$appSettingsKey1=$apiConnectionRuntimeUrl"
-az webapp config appsettings set --name $logicappsrefdataName --resource-group $resourceGroupName --settings "$appSettingsKey2=$serviceBusConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsrefdataName --resource-group $resourceGroupName --settings "$common_appSettingsKey=$apiConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsrefdataName --resource-group $resourceGroupName --settings "$servicebus_appSettingsKey=$serviceBusConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsrefdataName --resource-group $resourceGroupName --settings "$storage_appSettingsKey=$connectionStringFormatted"
 
 Write-Host "Updating App Settings for Logic App: $logicappsprocessorName..."
-az webapp config appsettings set --name $logicappsprocessorName --resource-group $resourceGroupName --settings "$appSettingsKey1=$apiConnectionRuntimeUrl"
-az webapp config appsettings set --name $logicappsprocessorName --resource-group $resourceGroupName --settings "$appSettingsKey2=$serviceBusConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsprocessorName --resource-group $resourceGroupName --settings "$common_appSettingsKey=$apiConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsprocessorName --resource-group $resourceGroupName --settings "$servicebus_appSettingsKey=$serviceBusConnectionRuntimeUrl"
+az webapp config appsettings set --name $logicappsprocessorName --resource-group $resourceGroupName --settings "$storage_appSettingsKey=$connectionStringFormatted"
 
 # Verify if the App Settings update was successful
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "Successfully updated App Settings with key '$appSettingsKey1' and '$appSettingsKey2' in Logic Apps."
+    Write-Host "Successfully updated App Settings with key '$common_appSettingsKey' and '$servicebus_appSettingsKey' and '$storage_appSettingsKey' in Logic Apps."
 } else {
     Write-Error "Error: Failed to update App Settings in Logic Apps."
     exit 1
